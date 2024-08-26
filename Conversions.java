e561aab257576239d8c18e3cd613e9025af89321
moonmantee <57419657+moonmantee@users.noreply.github.com>
2024-08-26 20:40:34 +0000

    Added 9 conversions to conversions.java

--- Conversions.java
+++ Conversions.java
@@ -17,11 +17,19 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. Feet to Miles");
+        System.out.println("8. Miles to Feet");
+        System.out.println("9. Yards to Feet");
+        System.out.println("10. Feet to Yards");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
-        int selection = keyboard.nextInt();
-        keyboard.nextLine();  //go past the endline and be ready for more input
+        int selection=-1;
+        do{
+            selection = keyboard.nextInt();
+            keyboard.nextLine();  //go past the endline and be ready for more input
+            System.out.println("Enter a valid number");
+        }while(selection>10 || selection<1);
 
         if (selection == 1)
         {
@@ -31,10 +39,70 @@ public class Conversions
             double fahrenheit = celsius * (9.0 / 5.0) + 32;
             System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees fahrenheit");
         }
-        if (selection == 2)
+        else if (selection == 2)
         {
-            //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+            System.out.println("Enter Fahrenheit: ");
+            double num = keyboard.nextDouble();
+            keyboard.nextLine();
+            double fahrenheit = (num-32)*5.0/9.0;
+            System.out.println(num + " degrees fahrenheit is " + fahrenheit + " degrees celsius");
         }
-        //add more if statements here.
+        else if(selection==3){
+            System.out.println("Enter Feet:");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double meter=num*0.3048;
+            System.out.println(num+" feet is "+meter+" meters");
+        }
+        else if(selection==4){
+            System.out.println("Enter Meters:");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet=num*3.28084;
+            System.out.println(num+" meters is "+feet+" feet");
+        }
+        else if(selection==5){
+            System.out.println("Enter Ounces:");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double milliliters=num*29.5735;
+            System.out.println(num +" ounces is "+milliliters+" Milliliters");
+        }
+        else if(selection==6){
+            System.out.println("Enter Milliliters:");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double ounces = num*0.033814;
+            System.out.println(num+" milliliters is "+ounces+" Ounces");
+        }
+        else if(selection==7){
+            System.out.println("Enter Feet: ");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double miles=num/5280;
+            System.out.println(num+" feet is "+miles+" miles");
+        }
+        else if(selection==8){
+            System.out.println("Enter Miles: ");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet=num*5280;
+            System.out.println(num+" Miles is "+feet+" Feet");
+        }
+        else if(selection==9){
+            System.out.println("Enter Yards: ");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet=num*3;
+            System.out.println(num+" Yards is "+feet+" Feet");
+        }
+        else{
+            System.out.println("Enter Feet: ");
+            double num=keyboard.nextDouble();
+            keyboard.nextLine();
+            double yards=num/3;
+            System.out.println(num+" Feet is "+yards+" Yards");
+        }
+        keyboard.close();
     }
 }
